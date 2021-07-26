e0ee98aa9a230f9fa1dce9936744717db575ee81
EliottVDW <82743027+Magoogabuka@users.noreply.github.com>
2021-07-26 18:52:36 +1000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,20 +1,44 @@
 #include "header.hpp"
 
-//function declarations
-
 int main()
 {
   Game game;
+  char menu;
+  bool pleaseQuit;
+  char choice;
 
-  cout<<"SUDOKU"<<endl;
-  cout<<"To input your choice, choose an x and a y coordinate."<<endl;
-  cout<<"Then input the number you wish to choose eg, 9"<<endl;
-  cout<<"If there is a zero, it means the space can be filled"<<endl;
+  do
+  {    
+      menu = game.menuScreen();
+    
+      if(menu == 'p' || menu == 'P')
+      {
+        game.playGame();
+        pleaseQuit = true;
+      }
+      else if(menu == 'q' || menu == 'Q')
+      {
+        pleaseQuit = true;
+      }
+      else if(menu == 'i' || menu == 'I')
+      {
+        cout<<"      ----------------SUDOKU-------------------\n\n";
+        cout<<"To input your choice, choose an x and a y coordinate.\n";
+        cout<<"Then input the number you wish to choose eg, 9\n";
+        cout<<"If there is a zero, it means the space can be filled\n\n";
+        cout<<"Would you like to return to the main menu? (y/n): ";
+        cin>>choice;
+        if(choice == 'Y' || choice == 'y')
+        {
+          pleaseQuit = false;
+        }
+        else
+        {
+          pleaseQuit = true;
+        }
 
-  //game.drawBoard();
-  //game.emptyRowCol();
-  //initBoard();
-  game.playGame();
+      }
+  } while(pleaseQuit == false);
 
   return 0;
 }
